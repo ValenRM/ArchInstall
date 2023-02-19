be6598b5f68862b907b880058175cd77eed41aed
Valen Ramos <valen.ramosmontero@gmail.com>
2023-02-19 10:30:20 -0300

    Added  'create_partitions' function

--- archinstall.sh
+++ archinstall.sh
@@ -30,9 +30,17 @@ InstallationType=0
 
 InstallationDisk=""
 
+#Installation Parameters
+
+USERNAME=""
+uPASSWORD=""
+rPASSWORD=""
+HOSTNAME=""
+BOOTLOADERID=""
+
 #Installer Steps
 
-EfiVars=0
+EfiVars=0 #this values are filled with $? variable -> to catch errors in the steps
 Partitions=0
 #other
 
@@ -43,6 +51,8 @@ function checkefivars() {
     echo -e "${GREEN}[*] ${WHITE}${BOLD}Checking EFI Variables ${RESET}${GREEN}${BOLD}<= ${RESET}${YELLOW}in progress..."
     ls $EfiVarsPath > /dev/null
     sleep 2
+
+    EfiVars=$?
 
     if [ $? -ne 0 ]; then
         clear
@@ -93,16 +103,16 @@ function set_installation_disk() {
       # Print the disk name and size
       echo -e "${CYAN}${BOLD}Disk       Size \n${RESET}$disk   $size\n\n"
     done
-    echo -e "${GREEN}${BOLD}[*]${RESET}Insert the disk name where you want to install Arch Linux. ${RED}${BOLD}Note: ${RESET}The entire disk will be formatted.\n"
+    echo -e "${GREEN}${BOLD}[*] ${RESET}Insert the disk name where you want to install Arch Linux. ${RED}${BOLD}Note: ${RESET}The entire disk will be formatted.\n"
     read -p "Disk: " InstallationDisk
     clear
-    echo -e "${GREEN}${BOLD}[*]${RESET}Specify the partition sizes for the installation. ${RED}${BOLD}Note: ${RESET}Leave empty if you want to use the default values.\n"
+    echo -e "${GREEN}${BOLD}[*] ${RESET}Specify the partition sizes for the installation. ${RED}${BOLD}Note: ${RESET}Leave empty if you want to use the default values.\n"
     read -p "Efi Partition (default +550M): " EfiSize
     clear
-    echo -e "${GREEN}${BOLD}[*]${RESET}Specify the partition sizes for the installation. ${RED}${BOLD}Note: ${RESET}Leave empty if you want to use the default values.\n"
+    echo -e "${GREEN}${BOLD}[*] ${RESET}Specify the partition sizes for the installation. ${RED}${BOLD}Note: ${RESET}Leave empty if you want to use the default values.\n"
     read -p "Swap Partition (default +4G): " SwapSize
     clear
-    echo -e "${GREEN}${BOLD}[*]${RESET}Specify the partition sizes for the installation. ${RED}${BOLD}Note: ${RESET}Leave empty if you want to use the default values.\n"
+    echo -e "${GREEN}${BOLD}[*] ${RESET}Specify the partition sizes for the installation. ${RED}${BOLD}Note: ${RESET}Leave empty if you want to use the default values.\n"
     read -p "Root Partition (default +125G): " RootSize
     clear
 
@@ -115,10 +125,71 @@ function set_installation_disk() {
     if [ ${#RootSize} -ne 0 ]; then
       dRootPartitionSize="$RootSize"
     fi
-
-    echo "$dEfiPartitionSize ; $dSwapPartitionSize ; $dRootPartitionSize"
 }
 
+function additional_parameters() {
+    installer_banner
+    echo -e "${GREEN}${BOLD}[*] ${RESET}Specify the following parameters for the installation.\n"
+    read -p "Hostname: " HOSTNAME
+    clear
+    installer_banner
+    echo -e "${GREEN}${BOLD}[*] ${RESET}Specify the following parameters for the installation.\n"
+    read -p "User:" USERNAME
+    clear
+    installer_banner
+    echo -e "${GREEN}${BOLD}[*] ${RESET}Specify the following parameters for the installation.\n"
+    read -sp "User Password:" uPASSWORD
+    clear
+    installer_banner
+    echo -e "${GREEN}${BOLD}[*] ${RESET}Specify the following parameters for the installation.\n"
+    read -sp "Root Password:" rPASSWORD
+    clear
+    installer_banner
+    echo -e "${GREEN}${BOLD}[*] ${RESET}Specify the following parameters for the installation. ${RED}${BOLD}Note: ${RESET}This is the name that will appear when you go into your boot manager.\n"
+    read -p "Bootloader ID:" USERNAME
+    clear
+    installer_banner
+    echo -e "${GREEN}${BOLD}[*] ${RESET}Starting Arch Linux Installation ${GREEN}${BOLD}<= ${RESET}${YELLOW}In Progress...\n"
+    sleep 2
+}
+
+function create_partitons() { 
+    echo -e "${YELLOW}${BOLD}[*] ${RESET}Creating Disk Partitions..."
+    PartitionErrors=0
+    #Fix: Value out of range notification are not counted as errors
+    (echo "g"; PartitionErrors+=$?; sleep 1; echo "n"; PartitionErrors+=$?; sleep 1; echo "1"; PartitionErrors+=$?; sleep 1; echo ""; PartitionErrors+=$?; sleep 1; echo "$dEfiPartitionSize"; PartitionErrors+=$?; sleep 1; echo "n"; PartitionErrors+=$?; sleep 1; echo "2"; PartitionErrors+=$?; sleep 1; echo""; PartitionErrors+=$?; sleep 1; echo "$dSwapPartitionSize"; PartitionErrors+=$?; sleep 1; echo "n"; PartitionErrors+=$?; sleep 1; echo "3"; PartitionErrors+=$?; sleep 1; echo ""; PartitionErrors+=$?; sleep 1; echo "$dRootPartitionSize"; PartitionErrors+=$?; sleep 1; echo "t"; PartitionErrors+=$?; sleep 1; echo "1"; PartitionErrors+=$?; sleep 1; echo "1"; PartitionErrors+=$?; sleep 1; echo "t"; PartitionErrors+=$?; sleep 1; echo "2"; PartitionErrors+=$?; sleep 1; echo "19"; PartitionErrors+=$?; sleep 1; echo "w"; PartitionErrors+=$?) | fdisk -W always $InstallationDisk > /dev/null 2>&1
+    PartitionErrors+=$?
+
+    mkfs.fat -F 32 ${InstallationDisk}1 > /dev/null 2>&1
+    exit_status=$?
+    PartitionErrors+=$exit_status
+    sleep 1
+
+    mkswap ${InstallationDisk}2 > /dev/null 2>&1
+    exit_status=$?
+    PartitionErrors+=$exit_status
+    sleep 1
+
+    swapon ${InstallationDisk}2 > /dev/null 2>&1
+    exit_status=$?
+    PartitionErrors+=$exit_status
+    sleep 1
+
+    mkfs.ext4 ${InstallationDisk}3 > /dev/null 2>&1
+    exit_status=$?
+    PartitionErrors+=$exit_status
+    sleep 1
+
+    if [ $PartitionErrors -ne 0 ]; then
+      Partitions=$PartitionErrors
+      echo -e "${RED}${BOLD}[*] ${RESET}Disks Partitions ${RED}${BOLD}<= Errors Ocurred"
+      exit 1
+    else
+      tput cuu1
+      tput ed
+      echo -e "${GREEN}${BOLD}[*] ${RESET}Disks Partitions"
+    fi
+}
 
 #Script Logic
 
@@ -131,6 +202,9 @@ elif [ "$InstallationType" -eq 2 ]; then
   clear
   installer_banner
   set_installation_disk
+  additional_parameters
+  #starting installation
+  create_partitons
 else
   clear
   defaultprompt
