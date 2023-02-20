fa9aea2099ba9387e503cbe541463d34351b2981
Valen Ramos <valen.ramosmontero@gmail.com>
2023-02-20 20:53:09 -0300

    Code Cleanup + Error Corrections

--- archinstall.sh
+++ archinstall.sh
@@ -18,6 +18,10 @@ RESET='\033[0m'
 HIDECURSOR="\e[?25l"
 SHOWCURSOR='\e[?25h'
 
+#Redirect to /dev/null
+
+TONULL="> /dev/null 2>&1"
+
 #Default Variables
 
 EfiVarsPath="/sys/firmware/efi/efivars"
@@ -58,6 +62,13 @@ Grub=0
 
 #Vital Functions
 
+function error_handler {
+    echo -e "\n${RED}${BOLD}Error: ${RESET}Command ${GREEN}'$1'${RESET} ${RED}${BOLD}failed${RESET} with exit status code of ${RED}${BOLD}$2"
+    exit 1
+}
+
+trap 'error_handler "$BASH_COMMAND" "$?"' ERR
+
 function cleanup() {
   echo -e "${SHOWCURSOR}"
 }
@@ -66,42 +77,36 @@ trap cleanup SIGINT
 trap cleanup EXIT
 
 uncomment_string() {
+    trap 'error_handler "$BASH_COMMAND" "$?"' ERR
     local str="$1"
     local path="$2"
     arch-chroot /mnt bash -c "sed -i 's/^#*\s*\(${str}\)/\1/' '${path}'"
 }
 
 function append_to_file() {
+    trap 'error_handler "$BASH_COMMAND" "$?"' ERR
     local text="$1"
     local file_path="$2"
     arch-chroot /mnt bash -c "echo '$text' >> '$file_path'"
 }
 
 function checkefivars() {
+    trap 'error_handler "$BASH_COMMAND" "$?"' ERR
     clear
     echo -e "${GREEN}[*] ${WHITE}${BOLD}Checking EFI Variables ${RESET}${GREEN}${BOLD}<= ${RESET}${YELLOW}in progress..."
     ls $EfiVarsPath > /dev/null
-    sleep 2
-
-    EfiVars=$?
-
-    if [ $? -ne 0 ]; then
-        clear
-        echo -e "${RED}${BOLD}[!] ${RESET}Installation Failed: No EFI variables have been found under $EfiVarsPath. Make sure you are booting using UEFI instead of BIOS or Legacy Mode"
-        exit 1
-    else
-        clear
-        echo -e "${GREEN}[*] ${WHITE}${BOLD}Checking EFI Variables ${RESET}${GREEN}${BOLD}<= ${RESET}${GREEN}Done."
-        sleep 2
-        clear
-    fi
+    sleep 1
+    clear
+    echo -e "${GREEN}[*] ${WHITE}${BOLD}Checking EFI Variables ${RESET}${GREEN}${BOLD}<= ${RESET}${GREEN}Done."
+    sleep 1
+    clear
 }
 
 function installer_banner() {
     echo -e "${CYAN}${BOLD}=========== Automatic Arch Linux Installation ===========${RESET}\n\n"
 }
 
-function defaultprompt() {
+function defaultprompt() { #Revise
     installer_banner
     echo -e "${WHITE}[1] Encrypted Installation"
     echo -e "${WHITE}[2] Normal Installation\n"
@@ -109,19 +114,16 @@ function defaultprompt() {
 }
 
 function set_installation_disk() {
+    trap 'error_handler "$BASH_COMMAND" "$?"' ERR
     disks=$(fdisk -l | grep -oP '/dev/\w+')
 
-    # Loop through each disk
     for disk in $disks
     do
-      # Ignore loop devices
       if [[ "$disk" == *loop* ]]; then
         continue
       fi
   
-      # Get the size of the disk in bytes
       size_bytes=$(blockdev --getsize64 $disk)
-      # Convert the size to GB or MB
       if [[ $size_bytes -gt 1073741824 ]]; then
         size=$(echo "scale=2;$size_bytes/1073741824" | bc)
         size="$size GB"
@@ -129,8 +131,6 @@ function set_installation_disk() {
         size=$(echo "scale=2;$size_bytes/1048576" | bc)
         size="$size MB"
       fi
-  
-      # Print the disk name and size
       echo -e "${CYAN}${BOLD}Disk       Size \n${RESET}$disk   $size\n\n"
     done
     echo -e "${GREEN}${BOLD}[*] ${RESET}Insert the disk name where you want to install Arch Linux. ${RED}${BOLD}Note: ${RESET}The entire disk will be formatted.\n"
@@ -158,6 +158,7 @@ function set_installation_disk() {
 }
 
 function additional_parameters() {
+    trap 'error_handler "$BASH_COMMAND" "$?"' ERR
     installer_banner
     echo -e "${GREEN}${BOLD}[*] ${RESET}Specify the following parameters for the installation.\n"
     read -p "Hostname: " HOSTNAME
@@ -176,7 +177,7 @@ function additional_parameters() {
     clear
     installer_banner
     echo -e "${GREEN}${BOLD}[*] ${RESET}Specify the following parameters for the installation. ${RED}${BOLD}Note: ${RESET}This is the name that will appear when you go into your boot manager.\n"
-    read -p "Bootloader ID:" USERNAME
+    read -p "Bootloader ID:" BOOTLOADERID
     clear
     installer_banner
     echo -e "${GREEN}${BOLD}[*] ${RESET}Specify the following parameters for the installation. ${RED}${BOLD}Syntax: ${RESET}<Region>/<City>.\n"
@@ -193,219 +194,148 @@ function additional_parameters() {
 }
 
 function create_partitons() { 
+    trap 'error_handler "$BASH_COMMAND" "$?"' ERR
+
     echo -e "${YELLOW}${BOLD}[*] ${RESET}Creating Disk Partitions..."
     PartitionErrors=0
-    #Fix: Value out of range notification are not counted as errors
-    (echo "g"; PartitionErrors+=$?; sleep 1; echo "n"; PartitionErrors+=$?; sleep 1; echo "1"; PartitionErrors+=$?; sleep 1; echo ""; PartitionErrors+=$?; sleep 1; echo "$dEfiPartitionSize"; PartitionErrors+=$?; sleep 1; echo "n"; PartitionErrors+=$?; sleep 1; echo "2"; PartitionErrors+=$?; sleep 1; echo""; PartitionErrors+=$?; sleep 1; echo "$dSwapPartitionSize"; PartitionErrors+=$?; sleep 1; echo "n"; PartitionErrors+=$?; sleep 1; echo "3"; PartitionErrors+=$?; sleep 1; echo ""; PartitionErrors+=$?; sleep 1; echo "$dRootPartitionSize"; PartitionErrors+=$?; sleep 1; echo "t"; PartitionErrors+=$?; sleep 1; echo "1"; PartitionErrors+=$?; sleep 1; echo "1"; PartitionErrors+=$?; sleep 1; echo "t"; PartitionErrors+=$?; sleep 1; echo "2"; PartitionErrors+=$?; sleep 1; echo "19"; PartitionErrors+=$?; sleep 1; echo "w"; PartitionErrors+=$?) | fdisk -W always $InstallationDisk > /dev/null 2>&1
-    PartitionErrors+=$?
+    (echo "g"; sleep 1; echo "n"; sleep 1; echo "1"; sleep 1; echo ""; sleep 1; echo "$dEfiPartitionSize"; sleep 1; echo "n"; sleep 1; echo "2"; sleep 1; echo""; sleep 1; echo "$dSwapPartitionSize"; sleep 1; echo "n"; sleep 1; echo "3"; sleep 1; echo ""; sleep 1; echo "$dRootPartitionSize"; sleep 1; echo "t"; sleep 1; echo "1"; sleep 1; echo "1"; sleep 1; echo "t"; sleep 1; echo "2"; sleep 1; echo "19"; sleep 1; echo "w") | fdisk -W always $InstallationDisk > /dev/null 2>&1
 
     mkfs.fat -F 32 ${InstallationDisk}1 > /dev/null 2>&1
-    exit_status=$?
     PartitionErrors+=$exit_status
     sleep 1
 
     mkswap ${InstallationDisk}2 > /dev/null 2>&1
-    exit_status=$?
     PartitionErrors+=$exit_status
     sleep 1
 
     swapon ${InstallationDisk}2 > /dev/null 2>&1
-    exit_status=$?
     PartitionErrors+=$exit_status
     sleep 1
 
     mkfs.ext4 ${InstallationDisk}3 > /dev/null 2>&1
-    exit_status=$?
     PartitionErrors+=$exit_status
-    sleep 1
 
-    if [ $PartitionErrors -ne 0 ]; then
-      Partitions=$PartitionErrors
-      echo -e "${RED}${BOLD}[*] ${RESET}Disks Partitions ${RED}${BOLD}<= Errors Ocurred"
-      exit 1
-    else
-      tput cuu1
-      tput ed
-      echo -e "${GREEN}${BOLD}[*] ${RESET}Disks Partitions"
-    fi
+    tput cuu1
+    tput ed
+    echo -e "${GREEN}${BOLD}[*] ${RESET}Disks Partitions"
 }
 
 
 function mount_fs() {
+  trap 'error_handler "$BASH_COMMAND" "$?"' ERR
   echo -e "${YELLOW}${BOLD}[*] ${RESET}Mounting File System..."
-  mount ${InstallationDisk}3 /mnt #> /dev/null 2>&1
-  FsMount+=$?
+  mount ${InstallationDisk}3 /mnt > /dev/null 2>&1
   sleep 1
   mkdir $BootFolder
-  FsMount+=$?
-  mount ${InstallationDisk}1 $BootFolder #> /dev/null 2>&1
-  FsMount+=$?
+  mount ${InstallationDisk}1 $BootFolder > /dev/null 2>&1
 
-  if [ $FsMount -ne 0 ]; then
-    echo -e "${RED}${BOLD}[*] ${RESET}File System Mount ${RED}${BOLD}<= Errors Ocurred"
-    exit 1
-  else
-    tput cuu1
-    tput ed
-    echo -e "${GREEN}${BOLD}[*] ${RESET}File System Mount"
-  fi
+  tput cuu1
+  tput ed
+  echo -e "${GREEN}${BOLD}[*] ${RESET}File System Mount"
 }
 
 function install_kernel() {
+  trap 'error_handler "$BASH_COMMAND" "$?"' ERR
   echo -e "${YELLOW}${BOLD}[*] ${RESET}Installing Linux Kernel..."
   pacstrap -K /mnt base linux linux-firmware >/dev/null 2>&1
-  if [ $? -ne 0 ]; then
-    echo -e "${RED}${BOLD}[*] ${RESET}Linux Kernel Installation ${RED}${BOLD}<= Errors Ocurred"
-    exit 1
-  else
-    tput cuu1
-    tput ed
-    echo -e "${GREEN}${BOLD}[*] ${RESET}Linux Kernel Installation"
-  fi
+  tput cuu1
+  tput ed
+  echo -e "${GREEN}${BOLD}[*] ${RESET}Linux Kernel Installation"
 }
 
 function create_fstab() {
+  trap 'error_handler "$BASH_COMMAND" "$?"' ERR
   echo -e "${YELLOW}${BOLD}[*] ${RESET}Creating FsTab..."
   genfstab -U /mnt > /mnt/etc/fstab >/dev/null 2>&1
   sleep 1
-    if [ $? -ne 0 ]; then
-    echo -e "${RED}${BOLD}[*] ${RESET}FsTab ${RED}${BOLD}<= Errors Ocurred"
-    exit 1
-  else
-    tput cuu1
-    tput ed
-    echo -e "${GREEN}${BOLD}[*] ${RESET}FsTab"
-  fi
+  tput cuu1
+  tput ed
+  echo -e "${GREEN}${BOLD}[*] ${RESET}FsTab"
 }
 
 function chroot_various() {
+  trap 'error_handler "$BASH_COMMAND" "$?"' ERR
   echo -e "${BLUE}${BOLD}   => ${RESET}arch-chroot Shell Enviroment"
   echo -e "${YELLOW}${BOLD}     [*] ${RESET}Configuring Time Zone..."
   arch-chroot /mnt bash -c "ln -sf /usr/share/zoneinfo/${TIMEZONE} /etc/localtime >/dev/null 2>&1; hwclock --systohc >/dev/null 2>&1"
-  if [ $? -ne 0 ]; then
-    echo -e "${RED}${BOLD}     [*] ${RESET}Time Zone ${RED}${BOLD}<= Errors Ocurred"
-    exit 1
-  else
-    tput cuu1
-    tput ed
-    echo -e "${GREEN}${BOLD}     [*] ${RESET}Time Zone"
-  fi
+  tput cuu1
+  tput ed
+  echo -e "${GREEN}${BOLD}     [*] ${RESET}Time Zone"
   echo -e "${YELLOW}${BOLD}     [*] ${RESET}Configuring Language..."
   uncomment_string "${LANGUAGE}.UTF-8 UTF-8" "/etc/locale.gen"
-  Language+=$?
 
   arch-chroot /mnt bash -c "locale-gen >/dev/null 2>&1"
-  Language+=$?
 
   append_to_file "LANG=${LANGUAGE}.UTF-8" "/etc/locale.conf"
-  Language+=$?
 
-  if [ $Language -ne 0 ]; then
-    echo -e "${RED}${BOLD}     [*] ${RESET}Language Configuration ${RED}${BOLD}<= Errors Ocurred"
-    exit 1
-  else
-    tput cuu1
-    tput ed
-    #tput civis
-    #tput cnorm
-    echo -e "${GREEN}${BOLD}     [*] ${RESET}Language Configuration"
-  fi
+  tput cuu1
+  tput ed
+  #tput civis
+  #tput cnorm
+  echo -e "${GREEN}${BOLD}     [*] ${RESET}Language Configuration"
 }
 
 function net_config() {
+  trap 'error_handler "$BASH_COMMAND" "$?"' ERR
   echo -e "${YELLOW}${BOLD}     [*] ${RESET}Configuring Network Settings..."
   append_to_file "${HOSTNAME}" "/etc/hostname"
-  Network+=$?
   hostspath="/etc/hosts"
   append_to_file "127.0.0.1     localhost" "${hostspath}"
   append_to_file "::1           localhost" "${hostspath}"
   append_to_file "127.0.1.1     ${HOSTNAME}.localhost     ${HOSTNAME}" "${hostspath}"
-  Network+=$?
-  sleep 3
-  if [ $Network -ne 0 ]; then
-    echo -e "${RED}${BOLD}     [*] ${RESET}Network Configuration ${RED}${BOLD}<= Errors Ocurred"
-    exit 1
-  else
-    tput cuu1
-    tput ed
-    echo -e "${GREEN}${BOLD}     [*] ${RESET}Network Configuration"
-  fi
+  sleep 1
+  tput cuu1
+  tput ed
+  echo -e "${GREEN}${BOLD}     [*] ${RESET}Network Configuration"
 }
 
 function create_users() {
+  trap 'error_handler "$BASH_COMMAND" "$?"' ERR
   echo -e "${YELLOW}${BOLD}     [*] ${RESET}Configuring Users..."
   arch-chroot /mnt bash -c "echo 'root:${rPASSWORD}' | chpasswd >/dev/null 2>&1"
-  Users+=$?
-  arch-chroot /mnt bash -c "useradd -m ${USERNAME} >/dev/null 2>&1"
-  Users+=$?
-  arch-chroot /mnt bash -c "echo '${USERNAME}:${uPASSWORD}' | chpasswd >/dev/null 2>&1"
-  Users+=$?
+  sleep 1
+  arch-chroot /mnt bash -c "useradd -m ${USERNAME} && echo '${USERNAME}:${uPASSWORD}' | chpasswd"
+  sleep 1
   arch-chroot /mnt bash -c "usermod -aG wheel,audio,video,optical,storage ${USERNAME} >/dev/null 2>&1"
-  Users+=$?
-  sleep 3
-  if ( $Users -ne 0); then
-    echo -e "${RED}${BOLD}     [*] ${RESET}User Configuration ${RED}${BOLD}<= Errors Ocurred"
-    exit 1
-  else
-    tput cuu1
-    tput cuu1
-    tput ed
-    echo -e "${GREEN}${BOLD}     [*] ${RESET}User Configuration"
-  fi
+  tput cuu1
+  tput ed
+  echo -e "${GREEN}${BOLD}     [*] ${RESET}User Configuration"
 }
 
 function install_sudo() {
+  trap 'error_handler "$BASH_COMMAND" "$?"' ERR
   echo -e "${YELLOW}${BOLD}     [*] ${RESET}Configuring Sudo..."
   arch-chroot /mnt bash -c "pacman -q --noconfirm -S sudo > /dev/null 2>&1"
-  sleep 5
-  Sudo+=$?
-  uncomment_string "%wheel ALL=(ALL:ALL) ALL" "/etc/sudoers"
   sleep 1
-  Sudo+=$?
-  if [ $Sudo -ne 0 ]; then
-    echo -e "${RED}${BOLD}     [*] ${RESET}Sudo Configuration ${RED}${BOLD}<= Errors Ocurred"
-    exit 1
-  else
-    tput cuu1
-    tput cuu1
-    tput ed
-    echo -e "${GREEN}${BOLD}     [*] ${RESET}Sudo Configuration"
-  fi
+  uncomment_string "%wheel ALL=(ALL:ALL) ALL" "/etc/sudoers"
+  tput cuu1
+  tput ed
+  echo -e "${GREEN}${BOLD}     [*] ${RESET}Sudo Configuration"
 }
 
 function install_grub() {
+  trap 'error_handler "$BASH_COMMAND" "$?"' ERR
   echo -e "${YELLOW}${BOLD}     [*] ${RESET}Installing Grub..."
-  arch-chroot /mnt bash -c "pacman -q --noconfirm -S grub efibootmgr dosfstools os-prober mtools networkmanager wpa_supplicant base-devel > /dev/null 2>&1"
-  Grub+=$?
+  arch-chroot /mnt bash -c "pacman -q --noconfirm -S grub efibootmgr dosfstools os-prober mtools networkmanager wpa_supplicant base-devel ${TONULL}"
   arch-chroot /mnt bash -c "grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=${BOOTLOADERID} --recheck > /dev/null 2>&1" #TODO: Support USB Installs
-  Grub+=$?
   arch-chroot /mnt bash -c "grub-mkconfig -o /boot/grub/grub.cfg > /dev/null 2>&1"
-  grub+=$?
-  if [ $Grub -ne 0 ]; then
-    echo -e "${RED}${BOLD}     [*] ${RESET}Grub Installation ${RED}${BOLD}<= Errors Ocurred"
-    exit 1
-  else
-    tput cuu1
-    tput ed
-    echo -e "${GREEN}${BOLD}     [*] ${RESET}Grub Installation"
-  fi
+  tput cuu1
+  tput ed
+  echo -e "${GREEN}${BOLD}     [*] ${RESET}Grub Installation"
 }
 
 function installer_cleanup() {
+  trap 'error_handler "$BASH_COMMAND" "$?"' ERR
   echo -e "${YELLOW}${BOLD}[*] ${RESET}Cleaning Up..."
   arch-chroot /mnt bash -c "systemctl enable NetworkManager > /dev/null 2>&1"
-  sleep 1
   arch-chroot /mnt bash -c "systemctl enable wpa_supplicant.service > /dev/null 2>&1"
-  sleep 1
-  arch-chroot /mnt bash -c "timedatectl set-ntp true > /dev/null 2>&1"
-  sleep 1
 
   umount -R /mnt > /dev/null 2>&1
 
   tput cuu1
   tput ed
   echo -e "${GREEN}${BOLD}[*]${RESET}${GREEN} Done.\n"
-
+  echo -e "${SHOWCURSOR}"
   read -p "Type r to reboot, or any key to exit the installer: " option
 
   if [ $option -ne "r" ]; then
@@ -415,6 +345,7 @@ function installer_cleanup() {
 
   clear
   echo -e "${RED}${BOLD}Warning: ${RESET}Remove the installation media."
+  echo -e "${HIDECURSOR}"
   sleep 10
   reboot 
 }
@@ -447,6 +378,5 @@ fi
 
 
 #TODO:
-#Sudo credentials match, but user is not being created (credentials mismatch)
 #Installer forces reboot
 #Support for USB installations
